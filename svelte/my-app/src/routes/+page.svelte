be34b7f300fb23571cc98389ed5819e5daa102cb
Javad Alamdar <49840579+javadalam@users.noreply.github.com>
2023-04-27 19:08:13 +0200

    add svelte tailwind

---
+++ svelte/my-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  <form action="">
+    <fieldset>info
+        <label for="name">name
+            <input type="text" name="name" id="name">
+        </label>
+    </fieldset>
+  </form>
